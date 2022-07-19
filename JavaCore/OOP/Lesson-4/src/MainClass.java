93697b462727472852ed6a8efe6ea8d9aacc62f2
Администратор <lmntrixuz@mail.ru>
2022-07-19 20:29:33 +0500

    Constructor

---
+++ JavaCore/OOP/Lesson-4/src/MainClass.java
@@ -0,0 +1,18 @@
+import uz.bepro.encapsulation.EncapsulationFinal;
+
+public class MainClass {
+
+    // staticni obyekt qilib chaqirib bolmaydi, static faqat class nomi bn chaqiriladi
+
+
+    public static void main(String[] args) {
+
+        EncapsulationFinal obj = new EncapsulationFinal();
+
+        obj.setId(456);
+        System.out.println(obj.getId());
+
+        System.out.println(EncapsulationFinal.getNextId());
+
+    }
+}
---
+++ JavaCore/OOP/Lesson-4/src/uz/bepro/encapsulation/EncapsulationFinal.java
@@ -0,0 +1,30 @@
+package uz.bepro.encapsulation;
+
+public class EncapsulationFinal {
+
+    //public EncapsulationFinal(){}
+    public EncapsulationFinal(String account, String api_url){
+        this.account = account;
+        API_URL = api_url;
+    }
+
+    private static int nextId = 1;
+    private  int id;
+
+    private final String account; // final togridan togri constructorga tasir qiladi
+    private final String API_URL;
+
+
+
+    public void setId(int id){
+        this.id = id;
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    public static int getNextId(){
+        return nextId;
+    }
+}
